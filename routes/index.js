17b11dfc3956e962d34014c34eb8ea237925677f
Harrisonkamau <kamauharrison87@gmail.com>
2016-09-17 21:57:10 +0300

    create user signup route && authenticate with passport-local

--- routes/index.js
+++ routes/index.js
@@ -26,8 +26,10 @@ router.get('/user/signup', function(req, res, next){
   res.render('user/signup', {csrfToken:req.csrfToken()});
 });
 
-router.post('/user/signup', function(req, res, next){
-  res.render('user/signup', {csrfToken:req.csrfToken()});
-});
+router.post('/user/signup', passport.authenticate('local,signup',{
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true
+}));
 
 module.exports = router;
