2fbdb8e98be8b249d3d36ce56f213e947c0d6aaa
Harrisonkamau <kamauharrison87@gmail.com>
2016-09-18 07:52:24 +0300

    set up express-validator

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator')
 
 var routes = require('./routes/index');
 
@@ -30,6 +31,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret: "very-secure-key", resave: false, saveUninitialized: false}));
 app.use(flash());
